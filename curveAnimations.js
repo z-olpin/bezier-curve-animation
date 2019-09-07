b772935028f48158f3fc8db65929ab3c1f412d28
Zachary Olpin <info@zacharyolp.in>
2019-09-06 21:44:57 -0600

    doin

---
+++ curveAnimations.js
@@ -0,0 +1,17 @@
+const ao = e => {
+  const rand = `${Math.random().toFixed(2)}, ${Math.random().toFixed(2)}, ${Math.random().toFixed(2)}, ${Math.random().toFixed(2)}`
+  rand.split(', ').map((e, i) => document.getElementById(`point_${1 + i}`).innerHTML = `${e}&nbsp;`)
+  const ball1 = document.getElementById("ball1")
+  const ball2 = document.getElementById("ball2")
+  const ball3 = document.getElementById("ball3")
+  const ball4 = document.getElementById("ball4")
+  const balls = [ball1, ball2, ball3, ball4]
+  balls.forEach(element => {
+    element.style['animation-play-state'] = 'paused'
+    element.style['animation-play-state'] = 'running'
+    element.style['animation-timing-function'] = `cubic-bezier(${rand})`
+  })
+}
+
+const butt = document.getElementById("butt")
+butt.addEventListener('click', ao)
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use('/', express.static('public'))
+app.use('/', express.static('./'))
 
 app.listen(3001, () => console.log('Gator app listening on port 3001!'));
